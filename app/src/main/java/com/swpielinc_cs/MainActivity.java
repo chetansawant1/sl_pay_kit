5bc7cf51265a7d7ad668774d270f7726771e96ad
chetansawant1 <chetansawant457@gmail.com>
2019-12-06 16:23:58 +0530

    y0

--- app/src/main/java/com/swpielinc_cs/MainActivity.java
+++ app/src/main/java/com/swpielinc_cs/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sl_pay_cs.slMotoTrnReq("1575612661360341","24517","INR","S","","","","SL0000000000003","B0D69595E51048E29AF85E7B9A75EBA8","10000003","","","","","","","","","","2012","123","4111 1111 1111 1111","Test","test@yahoo.in","9874563214",MainActivity.this);
     }
 }
